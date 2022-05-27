277ddecb4e43409f106d5f8b957f38d781bd2edd
h.toriyama <toriyamahiroto1128@gmail.com>
2022-05-27 09:43:56 +0900

    最小構成

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import TopPage from './pages/TopPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<TopPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/pages/TopPage.tsx
@@ -0,0 +1,7 @@
+const TopPage = () => {
+    return (
+        <h1>TopPage</h1>
+    );
+}
+
+export default TopPage;
