a5b13849649257eabecdccbe06f48a1d0aa938ce
h.toriyama <toriyamahiroto1128@gmail.com>
2022-06-08 17:51:58 +0900

    aタグにrel追加

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -6,7 +6,7 @@ const TopPage = ({connpassEvents}: TopPagePropsType) => {
             <h1>【connpass】本日のイベント</h1>
             {connpassEvents.map((event, index) => 
                 <div key={index}>
-                    <h2><a href={event.event_url} target="_blank">{event.title}</a></h2>
+                    <h2><a href={event.event_url} target="_blank" rel="noopener noreferrer">{event.title}</a></h2>
                     <p>開始時刻：{event.started_at.slice(11, 16)}</p>
                     <p>開始時刻：{event.ended_at.slice(11, 16)}</p>
                 </div>
