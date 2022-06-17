e6d34217cf712ce35154f8c4d27f862e3a841b8e
h.toriyama <toriyamahiroto1128@gmail.com>
2022-06-18 00:14:49 +0900

    ヘッダーとフッター追加

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container text-center">
+        <p className="text-muted">© shimashima</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,19 +1,24 @@
-import { Button, Col, Row } from "react-bootstrap";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 import { TopPagePropsType } from "../types/types";
 
 const TopPage = ({ connpassEvents }: TopPagePropsType) => {
   return (
     <div>
-      <h1>【connpass】本日のイベント</h1>
-      {connpassEvents.map((event, index) =>
-        <div key={index} className="card">
-          <div className="card-body">
-            <h2><a href={event.event_url} target="_blank" rel="noopener noreferrer">{event.title}</a></h2>
-            <p>開始時刻：{event.started_at.slice(11, 16)}</p>
-            <p>開始時刻：{event.ended_at.slice(11, 16)}</p>
+      <Header />
+      <div className="container">
+        <h1>【connpass】本日のイベント</h1>
+        {connpassEvents.map((event, index) =>
+          <div key={index} className="card">
+            <div className="card-body">
+              <h2><a href={event.event_url} target="_blank" rel="noopener noreferrer">{event.title}</a></h2>
+              <p>開始時刻：{event.started_at.slice(11, 16)}</p>
+              <p>開始時刻：{event.ended_at.slice(11, 16)}</p>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
+      <Footer />
     </div>
   );
 }
