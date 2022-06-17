5fc0b58cf3e631dbad626c262df90edcb68fcbd8
h.toriyama <toriyamahiroto1128@gmail.com>
2022-06-18 00:21:28 +0900

    見出し変更

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -7,11 +7,11 @@ const TopPage = ({ connpassEvents }: TopPagePropsType) => {
     <div>
       <Header />
       <div className="container">
-        <h1>【connpass】本日のイベント</h1>
+        <h1>Today's Events</h1>
         {connpassEvents.map((event, index) =>
           <div key={index} className="card">
             <div className="card-body">
-              <h2><a href={event.event_url} target="_blank" rel="noopener noreferrer">{event.title}</a></h2>
+              <h3><a href={event.event_url} target="_blank" rel="noopener noreferrer">{event.title}</a></h3>
               <p>開始時刻：{event.started_at.slice(11, 16)}</p>
               <p>開始時刻：{event.ended_at.slice(11, 16)}</p>
             </div>
