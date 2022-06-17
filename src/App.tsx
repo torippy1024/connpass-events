a88ce25a7267d6e433a09ec901d0ab41b74b8690
h.toriyama <toriyamahiroto1128@gmail.com>
2022-06-17 23:20:32 +0900

    インデントのスペースを2に合わせる

--- src/App.tsx
+++ src/App.tsx
@@ -6,40 +6,40 @@ import { ConnpassEventDataType } from './types/types';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ROUTER_BASENAME =
-    process.env.NODE_ENV === 'development' ? '/' : '/connpass-events';
+  process.env.NODE_ENV === 'development' ? '/' : '/connpass-events';
 
 function App() {
-    const [connpassEvents, setConnpassEvents] = useState<ConnpassEventDataType[]>([{
-        title: "",
-        description: "",
-        event_url: "",
-        started_at: "",
-        ended_at: ""
-    }]);
-    useEffect(() => {
-        fetch("https://shima-shima.site/connpass-api/today.json")
-            .then(res => res.json())
-            .then(data => data.events.reverse())
-            .then(events_ =>  events_.map((event_: any, index: any) => {
-                const event: ConnpassEventDataType = {
-                    title: event_.title,
-                    description: event_.description,
-                    event_url: event_.event_url,
-                    started_at: event_.started_at,
-                    ended_at: event_.ended_at   
-                }; 
-                return event;
-            }))
-            .then(events => setConnpassEvents(events))
-            .catch(err => "エラーが発生しました、ページをリロードして、もう一度トライしてください。");
-    }, [])
-    return (
-        <BrowserRouter basename={ROUTER_BASENAME}>
-            <Routes>
-                <Route path="/" element={<TopPage connpassEvents={connpassEvents}/>} />
-            </Routes>
-        </BrowserRouter>
-    );
+  const [connpassEvents, setConnpassEvents] = useState<ConnpassEventDataType[]>([{
+    title: "",
+    description: "",
+    event_url: "",
+    started_at: "",
+    ended_at: ""
+  }]);
+  useEffect(() => {
+    fetch("https://shima-shima.site/connpass-api/today.json")
+      .then(res => res.json())
+      .then(data => data.events.reverse())
+      .then(events_ => events_.map((event_: any, index: any) => {
+        const event: ConnpassEventDataType = {
+          title: event_.title,
+          description: event_.description,
+          event_url: event_.event_url,
+          started_at: event_.started_at,
+          ended_at: event_.ended_at
+        };
+        return event;
+      }))
+      .then(events => setConnpassEvents(events))
+      .catch(err => "エラーが発生しました、ページをリロードして、もう一度トライしてください。");
+  }, [])
+  return (
+    <BrowserRouter basename={ROUTER_BASENAME}>
+      <Routes>
+        <Route path="/" element={<TopPage connpassEvents={connpassEvents} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,20 +1,21 @@
+import { Button, Col, Row } from "react-bootstrap";
 import { TopPagePropsType } from "../types/types";
 
 const TopPage = ({ connpassEvents }: TopPagePropsType) => {
-    return (
-        <div>
-            <h1>【connpass】本日のイベント</h1>
-            {connpassEvents.map((event, index) =>
-                <div key={index} className="card">
-                    <div className="card-body">
-                        <h2><a href={event.event_url} target="_blank" rel="noopener noreferrer">{event.title}</a></h2>
-                        <p>開始時刻：{event.started_at.slice(11, 16)}</p>
-                        <p>開始時刻：{event.ended_at.slice(11, 16)}</p>
-                    </div>
-                </div>
-            )}
+  return (
+    <div>
+      <h1>【connpass】本日のイベント</h1>
+      {connpassEvents.map((event, index) =>
+        <div key={index} className="card">
+          <div className="card-body">
+            <h2><a href={event.event_url} target="_blank" rel="noopener noreferrer">{event.title}</a></h2>
+            <p>開始時刻：{event.started_at.slice(11, 16)}</p>
+            <p>開始時刻：{event.ended_at.slice(11, 16)}</p>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default TopPage;
