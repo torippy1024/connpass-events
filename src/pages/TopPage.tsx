723fe954b45642e586cc998f63d58fa6ce25ea10
h.toriyama <toriyamahiroto1128@gmail.com>
2022-06-18 22:35:23 +0900

    参加者数フィルター実装

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -8,10 +8,12 @@ const TopPage = ({ connpassEvents }: TopPagePropsType) => {
       <Header />
       <div className="container" id="top">
         <h1>Today's Events</h1>
-        {connpassEvents.map((event, index) =>
+        {connpassEvents.filter((event) => event.accepted >= 20)
+         .map((event, index) =>
           <div key={index} className="card">
             <div className="card-body">
               <h3><a href={event.event_url} target="_blank" rel="noopener noreferrer">{event.title}</a></h3>
+              <h4>参加人数：{event.accepted} / {event.limit}</h4>
               <p>開始時刻：{event.started_at.slice(11, 16)}</p>
               <p>開始時刻：{event.ended_at.slice(11, 16)}</p>
             </div>
