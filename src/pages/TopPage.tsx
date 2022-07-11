ed4873a443967c29a008fbefae6a48a4f1a6819b
h.toriyama <toriyamahiroto1128@gmail.com>
2022-07-11 12:49:53 +0900

    エラー解消

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 
-import { css } from "@emotion/react";
+// import { css } from "@emotion/react";
 import { useState } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
@@ -20,7 +20,7 @@ const TopPage = ({ connpassEvents, dayDisplay, setDayDisplay, eventsDates}: TopP
       <Header />
       <div className="container" id="top">
         <h1>{`${parseInt(eventsDates["days"][dayDisplay].slice(4, 6))}/${eventsDates["days"][dayDisplay].slice(6, 8)}`}開催のイベント</h1>
-        <p>s
+        <p>
           参加人数：
           <input
             type="range"
@@ -47,7 +47,7 @@ const TopPage = ({ connpassEvents, dayDisplay, setDayDisplay, eventsDates}: TopP
                 <div>{event.started_at.slice(11, 16)} ~ {event.ended_at.slice(11, 16)}</div>
             </div>
             <div className="card-body">
-              <h3 card-title><a href={event.event_url} target="_blank" rel="noopener noreferrer" className="text-decoration-none text-success">{event.title}</a></h3>
+              <h3 className="card-title"><a href={event.event_url} target="_blank" rel="noopener noreferrer" className="text-decoration-none text-success">{event.title}</a></h3>
               <div className="card-text text-muted">
                 <p className="text-truncate">{event.description.replace(/(<([^>]+)>)/gi, '')}</p>
               </div>
